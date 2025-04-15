72f3a465a1b03acadea609015a40419747f0bdb2
xLucyu <xlucyyu@gmail.com>
2025-04-15 18:49:56 +0200

    updated latest Leaderboard fetch

--- api/fetchId.py
+++ api/fetchId.py
@@ -16,19 +16,35 @@ def getData(url):
 
     except requests.exceptions.RequestException as e:
         raise ValueError(e)
+
+def getCurrentActiveLeaderboard(ids: dict, leaderboardApiKey: str) -> dict | None: 
+    
+    # this is used to check for the latest leaderboard which has players in them since they get released early
+
+    for currentApiIndex in range(len(ids)):
+        currentIndex = ids[currentApiIndex] 
+        if currentIndex.get(leaderboardApiKey) != 0:
+            return currentIndex
+
+    return None
         
 
-def getID(urls, index):
-    
+def getID(urls: dict, index: int) -> dict | None:
+     
     data = getData(urls.get("base", None))
     
     ids = data.get("body", None) 
-    selectedID = ids[index]
+    selectedID = ids[index] 
+    leaderboardApiKey = urls.get("totalscores", None)  
 
-    apiData = {
+    if leaderboardApiKey: # will check if if leaderboard has a key for total players 
+        selectedID = getCurrentActiveLeaderboard(ids, leaderboardApiKey)
+
+    if not selectedID:
+        return None 
+            
+    return {
         "Names": [name.get("name") for name in ids if ids], 
         "MetaData": selectedID.get(urls.get("extension", None)),
         "Data": selectedID 
-    } 
-
-    return apiData 
+    }  

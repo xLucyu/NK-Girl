7a1b379a4e1522d051e95cce89134686d60674af
xLucyu <xlucyyu@gmail.com>
2025-06-19 22:15:38 +0200

    added emotes for every ct banner/relic in the select menu

--- api/emojis.py
+++ api/emojis.py
@@ -3,7 +3,6 @@ from config import BOTTOKEN, BOTID
 
 def getEmojis(url=f"https://discord.com/api/v10/applications/{BOTID}/emojis") -> dict | None:
 
-
     try:
         headers = {
             "Authorization": f"Bot {BOTTOKEN}",

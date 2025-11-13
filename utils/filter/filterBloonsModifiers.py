b293e9d870d70dea91bcee7193c84aea53564220
xLucyu <xlucyyu@gmail.com>
2025-11-13 17:15:10 +0100

    some code refactor

--- utils/filter/filterBloonsModifiers.py
+++ utils/filter/filterBloonsModifiers.py
@@ -41,13 +41,15 @@ def filterModifiers(modifiers: dict, emotes: dict) -> list[str]:
         emoteKey = "" if modifier in NOKEYS else "Increase" if multiplier > 100 else "Decrease"
         emoteId = emotes.get(f"{modifier}{emoteKey}")
         formattedEmote = f"<:{modifier}{emoteKey}:{emoteId}>" #<:bossIncrease:1335339243345809478> example 
-
+        
         if modifier == "LeastCash":
             multiplier = f"${multiplier:,}"
+        elif modifier in ["MaxParagons", "MaxTowers"]:
+            multiplier = multiplier
         elif isinstance(multiplier, bool): 
             multiplier = ""
         else:
-            multiplier = f"% {int(multiplier)}"
+            multiplier = f"{int(multiplier)}% "
 
         formattedModifierList.append(f"{formattedEmote} {multiplier}{embedDisplayName}")
 

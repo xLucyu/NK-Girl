7171d76c30957898164883ad040f96032d4f5102
Lucy <xlucyyu@gmail.com>
2025-12-19 08:36:17 +0100

    Refactor getCurrentActiveEvent function
    Refactor getCurrentActiveEvent to use try-except for error handling.

--- cogs/currentEvent.py
+++ cogs/currentEvent.py
@@ -3,13 +3,14 @@ from typing import Tuple
 
 def getCurrentActiveEvent(mainData: NkData, currentTimeStamp: int) -> Tuple[int, Body]:
  
-    validEvents = [
-        (index, event)
-        for index, event in enumerate(mainData.body)
-        if event.end > currentTimeStamp
-    ]
-
-    if validEvents:
-        return min(validEvents, key = lambda event: event[1].end)
-
-    return 0
+    try:
+        return min(
+            (
+                (index, event)
+                for index, event in enumerate(mainData.body)
+                if event.end > currentTimeStamp
+            ), key=lambda event: event[1].end  
+        )
+     
+    except ValueError:
+        return 0, mainData.body[0]

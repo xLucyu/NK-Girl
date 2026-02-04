496d11a038e6209eb1cd534b79aeed264cb1d6cd
Lucy <xlucyyu@gmail.com>
2026-02-04 11:33:46 +0100

    Initialize profile module with imports and exports

---
+++ cogs/profile/__init__.py
@@ -0,0 +1,19 @@
+from .bossdetailsProfile import bossdetailsProfile
+from .bossProfile import bossProfile
+from .challengeProfile import challengeProfile
+from .leaderboardProfile import leaderboardProfile
+from .odysseyProfile import odysseyProfile
+from .raceProfile import raceProfile
+from .tileProfile import tileProfile
+from .timeProfile import timeProfile
+
+__all__ = [
+    "bossdetailsProfile",
+    "bossProfile",
+    "challengeProfile",
+    "leaderboardProfile",
+    "odysseyProfile",
+    "raceProfile",
+    "tileProfile",
+    "timeProfile"
+]

27dc25471e462ff03960e4968d139b88e5c07c17
Lucy <xlucyyu@gmail.com>
2026-02-05 13:52:40 +0100

    Create __init__.py for assets module
    Initialize the assets module and define exports.

---
+++ utils/assets/__init__.py
@@ -0,0 +1,32 @@
+from .bloonsModifiers import MODIFIERS
+from .bossTierHp import BOSSHP
+from .eventImages import (
+    RACE_IMAGE,
+    LEASTCASH_IMAGE,
+    LEASTTIERS_IMAGE,
+    CT_IMAGE,
+    BOSS_IMAGE,
+    ODYSSEY_IMAGE,
+    CHALLENGE_IMAGE,
+    MAPS_IMAGE
+)
+from .medals import MEDALS
+from .raceRounds import REGULAR, ABR
+from .towerCategories import CATEGORIES
+
+__all__ = [
+    "MODIFIERS",
+    "BOSSHP",
+    "RACE_IMAGE",
+    "LEASTCASH_IMAGE",
+    "LEASTTIERS_IMAGE",
+    "CT_IMAGE",
+    "BOSS_IMAGE",
+    "ODYSSEY_IMAGE",
+    "CHALLENGE_IMAGE",
+    "MAPS_IMAGE",
+    "MEDALS",
+    "REGULAR",
+    "ABR",
+    "CATEGORIES"
+]

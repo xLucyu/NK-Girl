1537e6813f0b5096cd1632900eded1ed383e98bb
Lucy <xlucyyu@gmail.com>
2026-02-05 13:46:26 +0100

    Update print statement from 'Hello' to 'Goodbye'

---
+++ utils/assets/eventImages.py
@@ -0,0 +1,144 @@
+BOSS_IMAGE = {
+    "standard": {
+        "Image": {
+            "Bloonarius": "https://static.wikia.nocookie.net/b__/images/d/d6/BloonariusPortrait.png/revision/latest?cb=20210726020747&path-prefix=bloons",
+            "Lych": "https://static.wikia.nocookie.net/b__/images/7/71/LychPortrait.png/revision/latest?cb=20211014000236&path-prefix=bloons",
+            "Vortex": "https://static.wikia.nocookie.net/b__/images/8/88/VortexPortrait.png/revision/latest?cb=20220413053047&path-prefix=bloons",
+            "Dreadbloon": "https://static.wikia.nocookie.net/b__/images/6/63/DreadbloonPortrait.png/revision/latest?cb=20221207232857&path-prefix=bloons",
+            "Phayze": "https://static.wikia.nocookie.net/b__/images/2/26/BTD6_Phayze_icon.png/revision/latest?cb=20230726031440&path-prefix=bloons",
+            "Blastapopoulos": "https://static.wikia.nocookie.net/b__/images/9/9c/BlastapopoulosPortrait.png/revision/latest?cb=20241009061900&path-prefix=bloons"
+        },
+        "Banner": {
+            "Bloonarius": "https://static-api.nkstatic.com/appdocs/4/assets/opendata/a13e4eb428f6e37c0f5c27b34209a373_bloonarius.png",
+            "Lych": "https://static-api.nkstatic.com/appdocs/4/assets/opendata/d09d65f1c741fc939994076e384c7339_lych.png",
+            "Vortex": "https://static-api.nkstatic.com/appdocs/4/assets/opendata/6c4ce1b7cf706e455215861e5ba6aee8_vortex.png",
+            "Dreadbloon": "https://static-api.nkstatic.com/appdocs/4/assets/opendata/ed8daf5c48556ea3bf7c333a5cbf7bb4_dreadbloon.png",
+            "Phayze": "https://static-api.nkstatic.com/appdocs/4/assets/opendata/f241841a0bcebc3aff8042ee0350b61d_phayze.png",
+            "Blastapopoulos": "https://static-api.nkstatic.com/appdocs/4/assets/opendata/cc5a96dd7fb35b5b4dfb9a316a90fc26_blastapopoulos.png"
+        }
+    },
+    "elite": {
+        "Image": {
+            "Bloonarius": "https://static.wikia.nocookie.net/b__/images/0/05/BloonariusPortraitElite.png/revision/latest?cb=20210726020748&path-prefix=bloons",
+            "Lych": "https://static.wikia.nocookie.net/b__/images/0/0f/LychPortraitElite.png/revision/latest?cb=20211014000247&path-prefix=bloons",
+            "Vortex": "https://static.wikia.nocookie.net/b__/images/b/bb/VortexPortraitElite.png/revision/latest?cb=20220413053048&path-prefix=bloons",
+            "Dreadbloon": "https://static.wikia.nocookie.net/b__/images/f/f7/DreadbloonPortraitElite.png/revision/latest?cb=20221207235238&path-prefix=bloons",
+            "Phayze": "https://static.wikia.nocookie.net/b__/images/7/7c/BTD6_Phayze_Elite_icon.png/revision/latest?cb=20230726031508&path-prefix=bloons",
+            "Blastapopoulos": "https://static.wikia.nocookie.net/b__/images/a/a6/BlastapopoulosPortraitElite.png/revision/latest?cb=20241009061900&path-prefix=bloons"
+        },
+        "Banner": {
+            "Bloonarius": "https://static-api.nkstatic.com/appdocs/4/assets/opendata/a13e4eb428f6e37c0f5c27b34209a373_bloonarius.png",
+            "Lych": "https://static-api.nkstatic.com/appdocs/4/assets/opendata/d09d65f1c741fc939994076e384c7339_lych.png",
+            "Vortex": "https://static-api.nkstatic.com/appdocs/4/assets/opendata/6c4ce1b7cf706e455215861e5ba6aee8_vortex.png",
+            "Dreadbloon": "https://static-api.nkstatic.com/appdocs/4/assets/opendata/ed8daf5c48556ea3bf7c333a5cbf7bb4_dreadbloon.png",
+            "Phayze": "https://static-api.nkstatic.com/appdocs/4/assets/opendata/f241841a0bcebc3aff8042ee0350b61d_phayze.png",
+            "Blastapopoulos": "https://static-api.nkstatic.com/appdocs/4/assets/opendata/cc5a96dd7fb35b5b4dfb9a316a90fc26_blastapopoulos.png"
+        }
+    }
+}
+
+RACE_IMAGE = "https://static.wikia.nocookie.net/b__/images/4/40/EventRaceIcon.png/revision/latest?cb=20210625211459&path-prefix=bloons"
+LEASTCASH_IMAGE = "https://static.wikia.nocookie.net/b__/images/b/bc/LeastCashIcon.png/revision/latest?cb=20220703150039&path-prefix=bloons"
+LEASTTIERS_IMAGE = "https://static.wikia.nocookie.net/b__/images/a/ac/LeastTiersIcon.png/revision/latest?cb=20220703150038&path-prefix=bloons"
+
+ODYSSEY_IMAGE = {
+    "easy": "https://static.wikia.nocookie.net/b__/images/9/9a/OdysseyModeEasyBtn.png/revision/latest?cb=20201219224324&path-prefix=bloons",
+    "medium": "https://static.wikia.nocookie.net/b__/images/6/66/OdysseyModeMediumBtn.png/revision/latest?cb=20201219224311&path-prefix=bloons",
+    "hard": "https://static.wikia.nocookie.net/b__/images/f/f4/OdysseyModeHardBtn.png/revision/latest?cb=20201219224317&path-prefix=bloons"
+}
+
+CHALLENGE_IMAGE = {
+    "daily": "https://static.wikia.nocookie.net/b__/images/b/bf/DailyChallengeBtn.png/revision/latest?cb=20200601042405&path-prefix=bloons",
+    "challenge": "https://static.wikia.nocookie.net/b__/images/8/86/CreateChallengeIcon.png/revision/latest?cb=20200601042401&path-prefix=bloons" 
+}
+
+CT_IMAGE = "https://www.bloonswiki.com/images/e/e7/BTD6_ContestedTerritoryEvent.png?20240609232601" 
+    
+MAPS_IMAGE = {
+    "Adoras Temple": "https://static.wikia.nocookie.net/b__/images/0/0a/AdorasTemple_No_UI.png/revision/latest?cb=20200519012904&path-prefix=bloons",
+    "Alpine Run": "https://static.wikia.nocookie.net/b__/images/0/07/AlpineRun_No_UI.png/revision/latest?cb=20200519012905&path-prefix=bloons",
+    "Ancient Portal": "https://static.wikia.nocookie.net/b__/images/b/bc/AncientPortal_No_UI.png/revision/latest?cb=20241009065716&path-prefix=bloons",
+    "Another Brick": "https://static.wikia.nocookie.net/b__/images/f/f4/AnotherBrick_No_UI.png/revision/latest?cb=20200519012906&path-prefix=bloons",
+    "Balance": "https://static.wikia.nocookie.net/b__/images/5/5a/Balance_No_UI.png/revision/latest?cb=20211112153122&path-prefix=bloons",
+    "Bazaar": "https://static.wikia.nocookie.net/b__/images/a/a4/Bazaar_No_UI.PNG/revision/latest?cb=20201113035009&path-prefix=bloons",
+    "Blons": "https://static.wikia.nocookie.net/b__/images/8/8e/Blons_No_UI.png/revision/latest?cb=20230203144448&path-prefix=bloons",
+    "Bloody Puddles": "https://static.wikia.nocookie.net/b__/images/3/31/BloodyPuddles_No_UI.png/revision/latest?cb=20200519012906&path-prefix=bloons",
+    "Bloonarius Prime": "https://static.wikia.nocookie.net/b__/images/9/97/BloonariusPrime_No_UI.png/revision/latest?cb=20210816054741&path-prefix=bloons",
+    "Candy Falls": "https://static.wikia.nocookie.net/b__/images/8/8e/CandyFalls_No_UI.png/revision/latest?cb=20200519012907&path-prefix=bloons",
+    "Cargo": "https://static.wikia.nocookie.net/b__/images/d/df/Cargo_No_UI.png/revision/latest?cb=20200519012907&path-prefix=bloons",
+    "Castle Revenge": "https://static.wikia.nocookie.net/b__/images/d/d7/CastleRevenge_No_UI.png/revision/latest?cb=20240408135401&path-prefix=bloons",
+    "Carved": "https://static.wikia.nocookie.net/b__/images/e/e9/Carved_No_UI.png/revision/latest?cb=20200519012908&path-prefix=bloons",
+    "Chutes": "https://static.wikia.nocookie.net/b__/images/d/d3/Chutes_No_UI.png/revision/latest?cb=20200519012908&path-prefix=bloons",
+    "Cornfield": "https://static.wikia.nocookie.net/b__/images/5/54/Cornfield_No_UI.png/revision/latest?cb=20200519012909&path-prefix=bloons",
+    "Covered Garden": "https://static.wikia.nocookie.net/b__/images/d/d0/CoveredGarden_No_UI.png/revision/latest?cb=20221012204437&path-prefix=bloons", 
+    "Cracked": "https://static.wikia.nocookie.net/b__/images/3/3e/Cracked_No_UI.png/revision/latest?cb=20200519012909&path-prefix=bloons",
+    "Cornfield": "https://static.wikia.nocookie.net/b__/images/e/e6/Cubism_No_UI.png/revision/latest?cb=20200519012910&path-prefix=bloons",
+    "Cubism": "https://static.wikia.nocookie.net/b__/images/e/e6/Cubism_No_UI.png/revision/latest?cb=20200519012910&path-prefix=bloons",
+    "Dark Castle": "https://static.wikia.nocookie.net/b__/images/1/1f/DarkCastle_No_UI.png/revision/latest?cb=20200519012911&path-prefix=bloons",
+    "Dark Dungeons": "https://static.wikia.nocookie.net/b__/images/8/8b/DarkDungeons_No_UI.png/revision/latest?cb=20230216084928&path-prefix=bloons",
+    "Dark Path": "https://static.wikia.nocookie.net/b__/images/6/6b/DarkPath_No_UI.png/revision/latest?cb=20231010074904&path-prefix=bloons",
+    "Downstream": "https://static.wikia.nocookie.net/b__/images/1/18/Downstream_No_UI.png/revision/latest?cb=20200519012911&path-prefix=bloons",
+    "Enchanted Glade": "https://static.wikia.nocookie.net/b__/images/b/b8/EnchantedGlade_No_UI.png/revision/latest?cb=20250205084253&path-prefix=bloons",
+    "Encrypted": "https://static.wikia.nocookie.net/b__/images/e/e5/MapSelectEncryptedButton.png/revision/latest?cb=20201016000424&path-prefix=bloons",
+    "End Of The Road": "https://static.wikia.nocookie.net/b__/images/e/e1/EndoftheRoad_No_UI.png/revision/latest?cb=20200519012912&path-prefix=bloons",
+    "Erosion": "https://static.wikia.nocookie.net/b__/images/1/12/Erosion_No_UI.png/revision/latest?cb=20230607074521&path-prefix=bloons",
+    "Firing Range": "https://static.wikia.nocookie.net/b__/images/4/42/FiringRange_No_UI.png/revision/latest?cb=20240701035311&path-prefix=bloons",
+    "Flooded Valley": "https://static.wikia.nocookie.net/b__/images/9/96/Flooded_Valley_No_UI.png/revision/latest?cb=20200908030039&path-prefix=bloons",
+    "Four Circles": "https://static.wikia.nocookie.net/b__/images/f/ff/FourCircles_No_UI.png/revision/latest?cb=20200519012913&path-prefix=bloons",
+    "Frozen Over": "https://static.wikia.nocookie.net/b__/images/0/00/FrozenOver_No_UI.png/revision/latest?cb=20200519012914&path-prefix=bloons",
+    "Geared": "https://static.wikia.nocookie.net/b__/images/d/df/Geared_No_UI.png/revision/latest?cb=20200519012914&path-prefix=bloons",
+    "Glacial Trail": "https://static.wikia.nocookie.net/b__/images/b/bf/GlacialTrail_No_UI.png/revision/latest?cb=20231206090901&path-prefix=bloons",
+    "Haunted": "https://static.wikia.nocookie.net/b__/images/e/e8/Haunted_No_UI.png/revision/latest?cb=20200519012915&path-prefix=bloons",
+    "Hedge": "https://static.wikia.nocookie.net/b__/images/c/cd/Hedge_No_UI.png/revision/latest?cb=20200519012916&path-prefix=bloons",
+    "High Finance": "https://static.wikia.nocookie.net/b__/images/4/4e/HighFinance_No_UI.png/revision/latest?cb=20200519013101&path-prefix=bloons",
+    "In The Loop": "https://static.wikia.nocookie.net/b__/images/d/d3/InTheLoop_No_UI.png/revision/latest?cb=20200519013102&path-prefix=bloons",
+    "Infernal": "https://static.wikia.nocookie.net/b__/images/d/d9/Infernal_No_UI.png/revision/latest?cb=20200519013101&path-prefix=bloons",
+    "Karts N Darts": "https://static.wikia.nocookie.net/b__/images/e/e6/KartsNDarts_No_UI.png/revision/latest?cb=20200519013102&path-prefix=bloons",
+    "Last Resort": "https://static.wikia.nocookie.net/b__/images/0/0f/Last_Resort_No_UI.png/revision/latest?cb=20241210132925&path-prefix=bloons",
+    "Lost Crevasse": "https://static.wikia.nocookie.net/b__/images/f/f4/Lost_Crevasse_No_UI.png/revision/latest?cb=20250827072050&path-prefix=bloons",
+    "Logs": "https://static.wikia.nocookie.net/b__/images/5/5d/Logs_No_UI.png/revision/latest?cb=20200519013103&path-prefix=bloons",
+    "Lotus Island": "https://static.wikia.nocookie.net/b__/images/9/9e/LotusIsland_No_UI.png/revision/latest?cb=20211008000755&path-prefix=bloons",
+    "Luminous Cove": "https://static.wikia.nocookie.net/b__/images/0/01/LuminousCove_No_UI.png/revision/latest?cb=20240801065346&path-prefix=bloons",
+    "Mesa": "https://static.wikia.nocookie.net/b__/images/d/d5/Mesa_No_UI.png/revision/latest?cb=20200903103358&path-prefix=bloons",
+    "Middle Of The Road": "https://static.wikia.nocookie.net/b__/images/f/f0/MiddleOfTheRoad_No_UI.png/revision/latest?cb=20230216084927&path-prefix=bloons",
+    "Midnight Mansion": "https://static.wikia.nocookie.net/b__/images/e/e0/Midnight_Mansion_No_UI.png/revision/latest?cb=20221012204419&path-prefix=bloons",
+    "Monkey Meadows": "https://static.wikia.nocookie.net/b__/images/e/e2/MonkeyMeadow_No_UI.png/revision/latest?cb=20200519013103&path-prefix=bloons",
+    "Moon Landing": "https://static.wikia.nocookie.net/b__/images/f/f8/MoonLanding_No_UI.png/revision/latest?cb=20200519013104&path-prefix=bloons",
+    "Muddy Puddles": "https://static.wikia.nocookie.net/b__/images/4/4c/MuddyPuddles_No_UI.png/revision/latest?cb=20200519013104&path-prefix=bloons",
+    "Off The Coast": "https://static.wikia.nocookie.net/b__/images/6/61/OffTheCoast_No_UI.png/revision/latest?cb=20200519013104&path-prefix=bloons",
+    "One Two Tree": "https://static.wikia.nocookie.net/b__/images/0/0b/OneTwoTree_No_UI.png/revision/latest?cb=20221208131615&path-prefix=bloons",
+    "#ouch": "https://static.wikia.nocookie.net/b__/images/0/09/Ouch_No_UI.png/revision/latest?cb=20200519013105&path-prefix=bloons",
+    "Park Path": "https://static.wikia.nocookie.net/b__/images/d/d8/ParkPath_No_UI.png/revision/latest?cb=20200519013106&path-prefix=bloons",
+    "Pats Pond": "https://static.wikia.nocookie.net/b__/images/9/96/PatsPond_No_UI.png/revision/latest?cb=20200519013106&path-prefix=bloons",
+    "Peninsula": "https://static.wikia.nocookie.net/b__/images/b/b7/Peninsula_No_UI.png/revision/latest?cb=20200518232444&path-prefix=bloons",
+    "Polyphemus": "https://static.wikia.nocookie.net/b__/images/2/2c/Polyphemus_No_UI.png/revision/latest?cb=20230404070617&path-prefix=bloons",
+    "Protect The Yacht": "https://static.wikia.nocookie.net/b__/images/8/8a/ProtectTheYacht_No_UI.png/revision/latest?cb=20240511170134&path-prefix=bloons",
+    "Quad": "https://static.wikia.nocookie.net/b__/images/6/69/Quad_No_UI.png/revision/latest?cb=20200519013107&path-prefix=bloons",
+    "Quarry": "https://static.wikia.nocookie.net/b__/images/5/53/Quarry_No_UI.png/revision/latest?cb=20221008184912&path-prefix=bloons",
+    "Quiet Street": "https://static.wikia.nocookie.net/b__/images/4/4e/QuietStreet_No_UI.png/revision/latest?cb=20211209004434&path-prefix=bloons",
+    "Rake": "https://static.wikia.nocookie.net/b__/images/2/23/Rake_No_UI.png/revision/latest?cb=20200519013107&path-prefix=bloons",
+    "Ravine": "https://static.wikia.nocookie.net/b__/images/3/38/Ravine_No_UI.png/revision/latest?cb=20211117040536&path-prefix=bloons",
+    "Resort": "https://static.wikia.nocookie.net/b__/images/3/38/Resort_No_UI.png/revision/latest?cb=20210930053044&path-prefix=bloons",
+    "Sanctuary": "https://static.wikia.nocookie.net/b__/images/6/60/Sanctuary_No_UI.png/revision/latest?cb=20210818052711&path-prefix=bloons",
+    "Scrapyard": "https://static.wikia.nocookie.net/b__/images/c/c4/Scrapyard_No_UI.png/revision/latest?cb=20220413173158&path-prefix=bloons",
+    "Skates": "https://static.wikia.nocookie.net/b__/images/5/58/MapSelectSkatesButton.png/revision/latest?cb=20201203035953&path-prefix=bloons",
+    "Spa Pits": "https://static.wikia.nocookie.net/b__/images/0/00/SpaPits_No_UI.png/revision/latest?cb=20250402071846&path-prefix=bloons",
+    "Spice Islands": "https://static.wikia.nocookie.net/b__/images/4/45/SpiceIslands_No_UI.png/revision/latest?cb=20200519013108&path-prefix=bloons",
+    "Spillway": "https://static.wikia.nocookie.net/b__/images/d/dd/Spillway_No_UI.png/revision/latest?cb=20200519013108&path-prefix=bloons",
+    "Spring Spring": "https://static.wikia.nocookie.net/b__/images/1/1c/SpringSpring_No_UI.png/revision/latest?cb=20200519013109&path-prefix=bloons",
+    "Streambed": "https://static.wikia.nocookie.net/b__/images/e/e7/Streambed_No_UI.png/revision/latest?cb=20200519013109&path-prefix=bloons",
+    "Sulfur Springs": "https://static.wikia.nocookie.net/b__/images/1/1c/Sulfur_Springs_No_UI.png/revision/latest?cb=20240207064930&path-prefix=bloons",
+    "Sunken Columns": "https://static.wikia.nocookie.net/b__/images/3/38/Sunken_Columns_No_UI.png/revision/latest?cb=20220217181915&path-prefix=bloons",
+    "Sunset Gulch": "https://static.wikia.nocookie.net/b__/images/f/fb/Sunset_Gulch_No_UI.png/revision/latest?cb=20250618071944&path-prefix=bloons",
+    "The Cabin": "https://static.wikia.nocookie.net/b__/images/b/b3/TheCabin_No_UI.png/revision/latest?cb=20211022045656&path-prefix=bloons",
+    "Tinkerton": "https://static.wikia.nocookie.net/b__/images/a/af/Tinkerton_No_UI.png/revision/latest?cb=20240529062923&path-prefix=bloons",
+    "Town Centre": "https://static.wikia.nocookie.net/b__/images/8/89/TownCenter_No_UI.png/revision/latest?cb=20200519013110&path-prefix=bloons",
+    "Tree Stump": "https://static.wikia.nocookie.net/b__/images/b/b4/TreeStump_No_UI.png/revision/latest?cb=20200519013110&path-prefix=bloons",
+    "Three Mines Around": "https://static.wikia.nocookie.net/b__/images/b/bf/Three_Mines_Around_No_UI.png/revision/latest?cb=20251015174111&path-prefix=bloons",
+    "Tricky Tracks": "https://www.bloonswiki.com/images/thumb/2/2f/BTD6_MapSelectTrickyTracksButton.png/250px-BTD6_MapSelectTrickyTracksButton.png",
+    "Underground": "https://static.wikia.nocookie.net/b__/images/5/59/Underground_No_UI.png/revision/latest?cb=20200519013124&path-prefix=bloons",
+    "Water Park": "https://static.wikia.nocookie.net/b__/images/4/46/WaterPark_No_UI.png/revision/latest?cb=20230726125928&path-prefix=bloons",
+    "Winter Park": "https://static.wikia.nocookie.net/b__/images/6/69/WinterPark_No_UI.png/revision/latest?cb=20200519013125&path-prefix=bloons",
+    "Workshop": "https://static.wikia.nocookie.net/b__/images/b/ba/Workshop_No_UI.png/revision/latest?cb=20200519013125&path-prefix=bloons",
+    "X Factor": "https://static.wikia.nocookie.net/b__/images/7/7c/MapSelectXFactorButton.png/revision/latest?cb=20201203040004&path-prefix=bloons"
+}

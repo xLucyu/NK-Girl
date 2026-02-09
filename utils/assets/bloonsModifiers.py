57956ad9df864449686397a06b9ba564756746a4
Lucy <xlucyyu@gmail.com>
2026-02-09 11:14:43 +0100

    Refactor modifiers to use Modifier class

--- utils/assets/bloonsModifiers.py
+++ utils/assets/bloonsModifiers.py
@@ -1,21 +1,74 @@
-MODIFIERS = {
-  "speedMultiplier": "Bloon Speed",
-  "moabSpeedMultiplier": "Moab Speed",
-  "bossSpeedMultiplier": "Boss Speed",
-  "regrowRateMultiplier": "Regrow Rate",
-  "bloons": "Ceramic Health",
-  "moabs": "Moab Health",
-  "boss": "Boss Health",
-  "allCamo": "All Camo",
-  "allRegen": "All Regen",
-  "MKDisabled": "MK Disabled",
-  "NoSelling": "No Selling",
-  "AbilityCoolDown": "Ability Cooldown Rate",
-  "NoContinues": "No Continues",
-  "MaxTowers": " Tower Limit",
-  "MaxParagons": " Paragon Limit",
-  "LeastCash": " Cash Limit",
-  "LeastTiers": " Tiers Max",
-  "PowersDisabled": "No Powers",
-  "RemoveableCost": "Removeable Cost"
-}
+from utils.dataclasses import MetaBody
+
+def buildModifiers(body: MetaBody) -> dict[str, Modifier]:
+  
+    return {
+        "speedMultiplier": Modifier(
+            label="Bloon Speed", 
+            api=body._bloonModifiers.speedMultiplier
+        ),
+        "moabSpeedMultiplier": Modifier(
+            label="Moab Speed", 
+            api=body._bloonModifiers.moabSpeedMultiplier
+        ),
+        "bossSpeedMultiplier": Modifier(
+            label="Boss Speed", 
+            api=body._bloonModifiers.bossSpeedMultiplier
+        ),
+        "regrowRateMultiplier": Modifier(
+            label="Regrow Rate", 
+            api=body._bloonModifiers.regrowRateMultiplier
+        ),
+        "bloons": Modifier(
+            label="Ceramic Health", 
+            api=body._bloonModifiers.healthMultipliers.bloons
+        ),
+        "moabs": Modifier(
+            label="Moab Health", 
+            api=body._bloonModifiers.healthMultipliers.moabs
+        ),
+        "boss": Modifier(
+            label="Boss Health", 
+            api=body._bloonModifiers.healthMultipliers.boss
+        ),
+        "disableMK": Modifier(
+            label="MK Disabled", 
+            api=body.disableMK
+        ),
+        "disableSelling": Modifier(
+            label="No Selling", 
+            api=body.disableSelling
+        ),
+        "abilityCooldownReductionMultiplier": Modifier(
+            label="Ability Cooldown Rate",
+            api=body.abilityCooldownReductionMultiplier
+        ),
+        "noContinues": Modifier(
+            label="No Continues", 
+            api=body.noContinues
+        ),
+        "maxTowers": Modifier(
+            label="Tower Limit",
+            api=body.maxTowers
+        ),
+        "maxParagons": Modifier(
+            label="Paragon Limit",
+            api=body.maxParagons
+        ),
+        "leastCashUsed": Modifier(
+            label="Cash Limit", 
+            api=body.leastCashUsed
+        ),
+        "leastTiersUsed": Modifier(
+            label="Tiers Max", 
+            api=body.leastTiersUsed
+        ),
+        "disablePowers": Modifier(
+            label="No Powers",
+            api=body.disablePowers
+        ),
+        "removeableCostMultiplier": Modifier(
+            label="Removeable Cost", 
+            api=body.removeableCostMultiplier
+        ),
+    }

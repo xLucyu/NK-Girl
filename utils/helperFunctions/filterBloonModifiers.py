624e0fedce45bc5a83bbf072c1e2df07e3005dd5
Lucy <xlucyyu@gmail.com>
2026-02-12 10:03:58 +0100

    Refactor filterModifiers function
    Refactor filterModifiers to return formatted labels and values.

--- utils/helperFunctions/filterBloonModifiers.py
+++ utils/helperFunctions/filterBloonModifiers.py
@@ -8,14 +8,23 @@ def filterModifiers(body: MetaBody, emojis: dict[str,  str], isCT: bool = False)
     
     modifierDict: dict[str, Modifier] = buildModifiers(body)
     
-    activeModifiers = {
-        key: mod.api
-        for key, mod in modifierDict.items()
-        if not (
-            (mod.api is False and isinstance(mod.api, bool))
-            or mod.api in [1, -1, 9999]
-            or (key == "maxParagons" and mod == 10)
-            or (key == "maxTowers" and mod == 0)
-        )
-    }
+    return {
+    (
+        f"${mod.label}" 
+        if mod.label == "Cash Limit"
+        else mod.label
+    ):
+    (
+        f"{int(mod.api * 100)}%"
+        if mod.hasKey
+        else mod.api
+    )
+    for key, mod in modifierDict.items()
+    if not (
+        (mod.api is False)
+        or mod.api in (1, -1, 9999)
+        or (key == "maxParagons" and mod.api == 10)
+        or (key == "maxTowers" and mod.api == 0)
+    )
+}
 

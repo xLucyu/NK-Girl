41f6dff3c46938d9ecb39bf6df025f8a117bd111
Lucy <xlucyyu@gmail.com>
2026-02-12 10:15:22 +0100

    Modify filterModifiers to support DcModel type
    Updated filterModifiers function to accept DcModel as a parameter type.

--- utils/helperFunctions/filterBloonModifiers.py
+++ utils/helperFunctions/filterBloonModifiers.py
@@ -1,30 +1,29 @@
+from tpying import Union
 from utils.dataclasses import (
     MetaBody, 
     Modifier,
-    buildModifiers 
+    buildModifiers,
+    DcModel
 )
 
-def filterModifiers(body: MetaBody, emojis: dict[str,  str], isCT: bool = False) -> list[str]:
-    
-    modifierDict: dict[str, Modifier] = buildModifiers(body)
-    
-    return {
-    (
-        f"${mod.label}" 
-        if mod.label == "Cash Limit"
-        else mod.label
-    ):
-    (
-        f"{int(mod.api * 100)}%"
-        if mod.hasKey
-        else mod.api
-    )
-    for key, mod in modifierDict.items()
-    if not (
-        (mod.api is False)
-        or mod.api in (1, -1, 9999)
-        or (key == "maxParagons" and mod.api == 10)
-        or (key == "maxTowers" and mod.api == 0)
-    )
-}
 
+def filterModifiers(body: Union[MetaBody, DcModel], emojis: dict[str,  str], isCT: bool = False) -> list[str]:
+    
+    modifierDict: dict[str, Modifier] = buildModifiers(body) # to do, if mode is CT, use a different builder with DcModel
+    
+    return [
+        (
+            f"{mod.api} {mod.label}"
+            if mod.label == "Cash Limit"
+            else f"{int(mod.api * 100)}% {mod.label}"
+            if mod.hasKey
+            else f"{mod.api} {mod.label}"
+        )
+        for key, mod in modifierDict.items()
+        if not (
+            (mod.api is False)
+            or mod.api in (1, -1, 9999)
+            or (key == "maxParagons" and mod.api == 10)
+            or (key == "maxTowers" and mod.api == 0)
+        )
+    ]

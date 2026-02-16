e58ba1e8b4d789ca2912c96f069f3a20e1bc6a07
Lucy <xlucyyu@gmail.com>
2026-02-16 15:53:00 +0100

    Add EventBody and Events dataclasses

---
+++ utils/dataclasses/events.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass 
+
+@dataclass
+class EventBody 
+  id: str 
+  type: str
+  name: str
+  start: int
+  end: int
+  url: str
+
+@dataclass
+class Events:
+  error: int 
+  success: bool
+  body: EventBody

31535e8e245fb55b2f43613599f986e6a3ec4d34
Lucy <xlucyyu@gmail.com>
2026-04-13 11:27:23 +0200

    Implement BaseEventCache class and interfaces

---
+++ src/event.manager/cache/base.ts
@@ -0,0 +1,41 @@
+export interface CurrentEventData<T, K> {
+  data: T;
+  metaData: Record<string, K> | null;
+}
+
+export interface EventCacheEntry<T, K> {
+  currentEvent: CurrentEventData<T, K>;
+  previousEvents: string[] | null;
+}
+
+export abstract class BaseEventCache<T extends { id: string }, K> {
+
+  protected cache: EventCacheEntry<T, K> | null = null;
+
+  protected abstract getCurrentActiveEvent(now: number): Promise<T>;
+  protected abstract getMetaData(event: T): Promise<Record<string, K> | null>;
+
+  protected async getPreviousEventIds(event: T): Promise<string[] | null> {
+    return null;
+  }
+
+  async check(): Promise<void> {
+    const now = Date.now();
+    const currentEvent = await this.getCurrentActiveEvent(now);
+
+    if (this.cache && this.cache.currentEvent.data.id === currentEvent.id) {
+      return;
+    }
+
+    const metaData = await this.getMetaData(currentEvent);
+    const previousEvents = await this.getPreviousEventIds(currentEvent);
+
+    this.cache = {
+      currentEvent: {
+        data: currentEvent,
+        metaData,
+      },
+      previousEvents,
+    };
+  }
+}

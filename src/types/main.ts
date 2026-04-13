3c35d368cba7149ad7cb7c678c81ee9c6b205812
Lucy <xlucyyu@gmail.com>
2026-04-13 12:33:49 +0200

    Refactor NkBody interface into BaseBody and derived interfaces

--- src/types/main.ts
+++ src/types/main.ts
@@ -1,32 +1,43 @@
-export interface NkBody {
+export interface BaseBody {
   id: string;
   name: string;
   start: number;
   end: number;
-  metadata?: string;
-  totalScores?: number;
-  leaderboard?: string;
-  bossType?: string;
-  metadataStandard?: string;
-  metadataElite?: string;
-  totalScores_standard?: number;
-  totalScores_elite?: number;
-  leaderboard_standard_players_1?: string;
-  leaderboard_elite_players_1?: string;
-  scoringType?: string;
-  normalScoringType?: string;
-  eliteScoringType?: string;
-  totalScores_player?: number;
-  totalScores_team?: number;
-  leaderboard_player?: string;
-  leaderboard_team?: string;
-  metadata_easy?: string;
-  metadata_medium?: string;
-  metadata_hard?: string;
-  createdAt?: number;
+}
+
+export interface BossBody extends BaseBody {
+  metadataStandard: string;
+  metadataElite: string;
+  totalScores_standard: number;
+  totalScores_elite: number;
+  leaderboard_standard_players_1: string;
+  leaderboard_elite_players_1: string;
+  scoringType: string;
+  normalScoringType: string;
+  eliteScoringType: string;
+  bossType: string;
+}
+
+export interface RaceBody extends BaseBody {
+  metadata: string;
+  totalScores: number;
+  leaderboard: string;
+}
+
+export interface CTBody extends BaseBody {
+  totalScores_player: number;
+  totalScores_team: number;
+  leaderboard_player: string;
+  leaderboard_team: string;
+}
+
+export interface OdysseyBody extends BaseBody {
+  metadata_easy: string;
+  metadata_medium: string;
+  metadata_hard: string;
 }
 
 export interface NkData {
   success: boolean;
-  body: NkBody[];
+  body: BaseBody[];
 }

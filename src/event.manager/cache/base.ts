f9907c438744e0cbac8c008ea42a728a02272b5f
xlucy <xlucyyu@gmail.com>
2026-04-14 21:13:44 +0200

    added utils and assets

--- src/event.manager/cache/base.ts
+++ src/event.manager/cache/base.ts
@@ -1,4 +1,4 @@
-import { BaseBody, EventBody } from "../../types";
+import { BaseBody, EventBody } from "../../utils/types";
 
 export interface CurrentEventData<T, K> {
   data: T;
@@ -10,37 +10,38 @@ export interface EventCacheEntry<T, K> {
   previousEvents: string[] | null;
 }
 
-export abstract class BaseEventCache<T extends (BaseBody | EventBody),K = never> {
+export abstract class BaseEventCache<T extends (BaseBody | EventBody), K = never> {
 
   protected cache: EventCacheEntry<T, K> | null = null;
 
-  protected async getPreviousEventIds(event: T): Promise<string[] | null> {
+  protected getPreviousEventIds(events: T[]): string[] | null {
     return null;
   }
 
+  protected abstract getEventData(): Promise<T[]>;
+  protected abstract getCurrentActiveEvent(events: T[], now: number): T;
+  protected abstract getMetaData(event: T): Promise<Record<string, K> | null | undefined>;
+
   async check(): Promise<void> {
 
-    const now = Date.now();
-    const currentEvent = await this.getCurrentActiveEvent(now);
+    const now = Date.now(); // get date time
 
-    if (this.cache && this.cache.currentEvent.data.id === currentEvent.id) {
-      return;
-    }
+    const events = await this.getEventData(); // get the the event body
 
-    const metaData = await this.getMetaData(currentEvent);
-    const previousEvents = await this.getPreviousEventIds(currentEvent);
+    const currentEvent = this.getCurrentActiveEvent(events, now); // get the ongoing event
+
+    if (this.cache && this.cache.currentEvent.data.id === currentEvent.id) return;
+
+    const metaData = await this.getMetaData(currentEvent); // get meta data for the event, if present 
+
+    const previousEvents = this.getPreviousEventIds(events); // get previous events for later select menu
 
     this.cache = {
       currentEvent: {
         data: currentEvent,
-        metaData,
+        metaData: metaData ?? null 
       },
-      previousEvents,
-    };
+      previousEvents
+    }
   }
-
-  protected abstract getCurrentActiveEvent(now: number): Promise<T>;
-
-  protected abstract getMetaData(event: T): Promise<Record<string, K> | null>;
-  
 }
---
+++ src/utils/assets/index.ts
@@ -0,0 +1,6 @@
+export * from "./boss.values";
+export * from "./event.images";
+export * from "./event.urls";
+export * from "./medals";
+export * from "./race.rounds";
+export * from "./towers.categories";

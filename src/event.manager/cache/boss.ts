0cd95352e7f89e308344963c4bc4cb8c485dfe48
Lucy <xlucyyu@gmail.com>
2026-04-16 10:55:50 +0200

    Enhance BossCache with event and metadata retrieval

--- src/event.manager/cache/boss.ts
+++ src/event.manager/cache/boss.ts
@@ -1,7 +1,7 @@
 import { BossBody, MetaData, NkData } from "../../utils/types";
 import { BaseEventCache } from "./base";
 import { getData } from "../../api/wrapper";
-import { URLS } from "../../utils/assets";
+import { URLS, BOSSDIFFICULTIES, BossDifficulty } from "../../utils/assets";
 
 export class BossCache extends BaseEventCache<BossBody, MetaData> {
 
@@ -9,6 +9,27 @@ export class BossCache extends BaseEventCache<BossBody, MetaData> {
 
        const data = await getData<NkData<BossBody>>(URLS.Boss.base)
        return data.body 
-       
+    }
+
+    protected getCurrentActiveEvent(events: BossBody[], now: number): BossBody {
+
+        const currentEvent = events.find((event) => event.end > now);
+
+        if (!currentEvent) throw new Error();
+        return currentEvent;
+    }
+
+    protected async getMetaData(event: BossBody): Promise<Record<BossDifficulty, MetaData> | null | undefined> {
+
+        const entries = await Promise.all(
+            BOSSDIFFICULTIES.map(async (difficulty) => {
+                const url = difficulty === "Standard"
+                    ? event.metadataStandard
+                    : event.metadataElite;
+                const data = await getData<MetaData>(url);
+                return [difficulty, data] as const;
+            })
+        )
+        return Object.fromEntries(entries) as Record<BossDifficulty, MetaData>;
     }
 }

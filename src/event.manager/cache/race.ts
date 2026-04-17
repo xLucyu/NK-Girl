81eef8dd9a5954acac7faa9998d5105b95827229
Lucy <xlucyyu@gmail.com>
2026-04-17 11:56:35 +0200

    Implement RaceCache class for race event management

---
+++ src/event.manager/cache/race.ts
@@ -0,0 +1,36 @@
+import { getData } from "../../api/wrapper";
+import { URLS } from "../../utils/assets";
+import { MetaBody, MetaData, NkData, RaceBody } from "../../utils/types";
+import { BaseEventCache, EventType } from "./base";
+
+export class RaceCache extends BaseEventCache<RaceBody, MetaBody> {
+
+    protected eventType: EventType = EventType.Race;
+
+    protected async getEventData(): Promise<RaceBody[]> {
+
+        const data = await getData<NkData<RaceBody>>(URLS.Race.base);
+        return data.body;
+    }
+
+    protected getCurrentActiveEvent(events: RaceBody[], now: number, firstUse: boolean): RaceBody {
+
+        let currentEvent: RaceBody | undefined;
+        
+        if (firstUse) {
+            currentEvent = events[0];
+        } else {
+            currentEvent = events.find((event) => event.end > now);
+        }
+
+        if (!currentEvent) throw new Error();
+        return currentEvent;
+    }
+
+    protected async getMetaData(event: RaceBody): Promise<MetaBody> {
+        
+        const data = await getData<MetaData>(event.metadata);
+        return data.body as MetaBody;
+    }
+
+}

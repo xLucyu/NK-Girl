9146d86ddf89cdf807cae6a3d723cc35d57b9ec8
xlucy <xlucyyu@gmail.com>
2026-04-24 20:26:41 +0200

    finished event manager i think

--- src/event.manager/cache/race.ts
+++ src/event.manager/cache/race.ts
@@ -1,17 +1,12 @@
 import { getData } from "../../api/wrapper";
 import { URLS } from "../../utils/assets";
-import { MetaBody, MetaData, NkData, RaceBody } from "../../utils/types";
+import { MetaBody, MetaData, RaceBody } from "../../utils/types";
 import { BaseEventCache, EventType } from "./base";
 
 export class RaceCache extends BaseEventCache<RaceBody, MetaBody> {
 
     protected eventType: EventType = EventType.Race;
-
-    protected async getEventData(): Promise<RaceBody[]> {
-
-        const data = await getData<NkData<RaceBody>>(URLS.Race.base);
-        return data.body;
-    }
+    protected url = URLS.Race;
 
     protected getCurrentActiveEvent(events: RaceBody[], now: number, firstUse: boolean): RaceBody {
 
@@ -27,13 +22,10 @@ export class RaceCache extends BaseEventCache<RaceBody, MetaBody> {
         return currentEvent;
     }
 
+    
     protected async getMetaData(event: RaceBody): Promise<MetaBody> {
         
         const data = await getData<MetaData>(event.metadata);
         return data.body as MetaBody;
-    }
-
-    protected getPreviousEventIds(events: RaceBody[]): string[] | null {
-        return events.map((event) => event.id);
     }
 }

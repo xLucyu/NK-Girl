3651a5e7fe4568e67bdb171c1e1eae2a01ecae3c
Lucy <xlucyyu@gmail.com>
2026-04-30 15:49:47 +0200

    Refactor BossCommand to handle boss data retrieval

--- src/commands/boss/boss.command.ts
+++ src/commands/boss/boss.command.ts
@@ -1,9 +1,29 @@
-import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { SlashCommandBuilder } from "discord.js";
+import { eventManager } from "../../event.manager/manager";
+import { EventCacheEntry, EventType } from "../../event.manager/cache/base";
+import { BaseCommand } from "../base.command";
+import { BossDifficulties, BossDifficulty } from "../../event.manager/cache";
+import { BossBody, MetaBody } from "../../utils/types";
 
-export const data = new SlashCommandBuilder()
-  .setName("ping")
-  .setDescription("Replies with Pong!");
+export class BossCommand extends BaseCommand<BossBody, Record<BossDifficulty, MetaBody> {
 
-export async function execute(interaction: CommandInteraction) {
-  return interaction.reply("Pong!");
+  public commandData = new SlashCommandBuilder()
+    .setName("boss")
+    .setDescription("shows the boss data.")
+    .addStringOption((option) =>
+    option 
+      .setName("difficulty")
+      .setDescription("Choose a difficulty")
+      .setRequired(false)
+      .addChoices(
+        ...BossDifficulties.map((difficulty) => ({
+          name: difficulty,
+          value: difficulty 
+        }))
+      )
+    );
+
+  protected getProfile() {
+    
+  }
 }

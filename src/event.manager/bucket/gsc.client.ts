12b8e91cb848be2c7c0023bfd8a36c3a1503d97d
Lucy <xlucyyu@gmail.com>
2026-05-08 14:48:31 +0200

    Update gsc.client.ts

--- src/event.manager/bucket/gsc.client.ts
+++ src/event.manager/bucket/gsc.client.ts
@@ -3,20 +3,21 @@ import { Storage, Bucket } from "@google-cloud/storage";
 export class GscClient {
 
     private storage: Storage;
+    private bucket: Bucket
 
-    constructor(filename: string) {
+    constructor(filename: string, bucketName: string) {
         this.storage = new Storage({ keyFilename: filename });
+        this.bucket = this.storage.bucket(bucketName);
     }
+    
 
-    public getBucket(bucketName: string): Bucket {
-        return this.storage.bucket(bucketName);
-    }
-
-    public async write(bucket: Bucket, path: string, data: unknown): Promise<void> {
+    public async write(path: string, data: unknown): Promise<void> {
         
-        await bucket.file(path).save(JSON.stringify(data, null, 2), {
+        await this.bucket.file(path).save(JSON.stringify(data, null, 2), {
             contentType: "application/json",
             resumable: false,
         });
     }
 }
+
+const gsc = new GscClient("serviceAccount.json", "btd6api");

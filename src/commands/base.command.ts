68a13e7c2f38f88bb87c0aa4f0c6def45093a68b
Lucy <xlucyyu@gmail.com>
2026-05-18 14:27:27 +0200

    Refactor BaseCommand class and update execute method

--- src/commands/base.command.ts
+++ src/commands/base.command.ts
@@ -1,15 +1,39 @@
-import type { JSX } from "react";
-import { ChatInputCommandInteraction, SlashCommandOptionsOnlyBuilder } from "discord.js";
+import { JSX } from "react";
+import { 
+    AttachmentBuilder,
+    ChatInputCommandInteraction,
+    SlashCommandOptionsOnlyBuilder
+} from "discord.js";
 import { EventCacheEntry } from "@manager/cache";
+import { render } from "@components/render";
+
+export interface BaseProfileProps {
+
+}
 
 export abstract class BaseCommand<T, K> {
 
     public abstract commandData: SlashCommandOptionsOnlyBuilder;
 
-    public async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+    public async execute(interaction: ChatInputCommandInteraction) {
+
+        const eventProps = this.getEventProps();
+
+        if (!eventProps) throw new Error();
+
+        const profile = this.getProfile(interaction, eventProps);
+        const buffer = await render(profile);
+
+        const attachment = new AttachmentBuilder(buffer, { name: "image.png" });
+
+        return interaction.reply({
+            files: [attachment]
+        });
     }
 
-    protected abstract getProfile(): JSX.Element;
     protected abstract getEventProps(): EventCacheEntry<T, K> | null;
-
+    protected abstract getProfile(
+        interaction: ChatInputCommandInteraction,
+        eventProps: EventCacheEntry<T,K>
+    ): JSX.Element;
 }

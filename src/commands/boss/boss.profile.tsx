711697232003b362f052df319283b93931474453
Lucy <xlucyyu@gmail.com>
2026-05-18 14:27:15 +0200

    Update BossProfile to use props
    Refactor BossProfile component to accept props for event, metaData, and difficulty.

--- src/commands/boss/boss.profile.tsx
+++ src/commands/boss/boss.profile.tsx
@@ -1,9 +1,32 @@
+import { BossDifficulty } from "@manager/cache";
+import { BossBody, MetaBody } from "@utils/types";
 import { JSX } from "react";
 
-export function BossProfile(): JSX.Element {
-    return (
-        <div>
+export interface BossProfileProps {
+    event: BossBody;
+    metaData: MetaBody;
+    difficulty: BossDifficulty;
+}
 
-        </div>
-    )
+export function BossProfile({
+  event,
+  metaData,
+  difficulty,
+}: BossProfileProps): JSX.Element {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        height: "100%",
+        padding: 40,
+        backgroundColor: "white",
+      }}
+    >
+      <h1>{event.name}</h1>
+      <p>Difficulty: {difficulty}</p>
+      <p>Starting Cash: {metaData.startingCash}</p>
+    </div>
+  );
 }

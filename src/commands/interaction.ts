f733022411e5ad6b5af5eead9f4fd2e4f1f84ed7
Lucy <xlucyyu@gmail.com>
2026-05-20 11:41:54 +0200

    Implement error handling in command listener
    Added error handling for command execution.

--- src/commands/interaction.ts
+++ src/commands/interaction.ts
@@ -1,11 +1,18 @@
 import { Interaction } from "discord.js";
 import { commands } from ".";
+import { sendCommandError } from "@utils/error.handler/error.reply";
 
 export async function listener(interaction: Interaction) {
 
     if (!interaction.isChatInputCommand()) return;
 
-    const command = commands[interaction.commandName as keyof typeof commands];
+    const command = commands[interaction.commandName as keyof typeof commands]
 
-    await command.execute(interaction);
+    if (!command) return;
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await sendCommandError(interaction, error);
+    }
 }

8c5444914ea4b94ff38112e5f8d47ceff22881f8
xlucy <xlucyyu@gmail.com>
2026-05-23 19:01:37 +0200

    revamp towers function

--- src/utils/helpers/towers.ts
+++ src/utils/helpers/towers.ts
@@ -1,6 +1,8 @@
 import { CATEGORIES } from "@utils/assets/constants";
 import type { Tower } from "../types";
 
+type TowerCategories = Record<keyof typeof CATEGORIES, Array<Tower & { crossPaths: [number, number, number ]}>>
+
 function getCrossPaths(tower: Tower): [number, number, number] {
     return [
         Math.max(0, 5 - (tower.path1NumBlockedTiers !== -1 ? tower.path1NumBlockedTiers : 5)),
@@ -9,7 +11,12 @@ function getCrossPaths(tower: Tower): [number, number, number] {
     ]
 }
 
-export function getTowers(towers: Tower[]) {
+function getTowerCategory(towerName: string): keyof typeof CATEGORIES {
+    return Object.entries(CATEGORIES).find(([_, towers]) => 
+        towers.includes(towerName))?.[0] as keyof typeof CATEGORIES;
+}
+
+export function getTowers(towers: Tower[]): TowerCategories {
 
   const towerCategories = {
     Heroes: [],
@@ -17,19 +24,19 @@ export function getTowers(towers: Tower[]) {
     Military: [],
     Magic: [],
     Support: [],
-  } as Record<keyof typeof CATEGORIES, Array<Tower & { crossPaths: [number, number, number ]}>>;
+  } as TowerCategories;
 
+  for (const tower of towers) {
 
-    for (const tower of towers.filter((tower) => tower.max !== 0)) {
-        for (const category of Object.keys(CATEGORIES) as Array<keyof typeof CATEGORIES>) {
-            if (tower.tower in CATEGORIES[category]) {
-                towerCategories[category].push({
-                ...tower,
-                crossPaths: getCrossPaths(tower),
-                });
-            break;
-            }
-        }
-    }
+    if (tower.max === 0) continue;
+
+    const category = getTowerCategory(tower.tower);
+
+    towerCategories[category].push({
+        ...tower,
+        crossPaths: getCrossPaths(tower)
+    });
+  }
+
   return towerCategories;
 }

dd058f733adf3f51495a24bcbd051fff5db2a260
Lucy <xlucyyu@gmail.com>
2026-06-22 09:23:48 +0200

    Add CollectionProfile component with props interface

---
+++ src/commands/collection/collection.profile.tsx
@@ -0,0 +1,18 @@
+import { JSX } from "react";
+import { EventBody, InstaSchedule } from "@utils";
+
+export interface BossProfileProps {
+    event: EventBody;
+    metaData: InstaSchedule;
+}
+
+export function CollectionProfile({
+  event,
+  metaData,
+}: BossProfileProps): JSX.Element {
+  return (
+    <div>
+
+    </div>
+  )
+}

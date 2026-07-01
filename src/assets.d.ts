e89e4694aedc5ec2d91d2b39d2484dd9b9f86c71
Lucy <xlucyyu@gmail.com>
2026-07-01 11:26:40 +0200

    Add JSX type definition for React

--- src/assets.d.ts
+++ src/assets.d.ts
@@ -1,3 +1,11 @@
+import type { JSX as ReactJSX } from "react";
+
+declare global {
+  namespace JSX {
+    type Element = ReactJSX.Element;
+  }
+}
+
 declare module "*.png" {
   const src: string;
   export default src;

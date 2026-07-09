f3cd52a2789940c9e54576fd7b4865e64d615ddb
Lucy <xlucyyu@gmail.com>
2026-07-09 09:30:25 +0200

    Refactor RaceProfile component with new imports and layout

--- src/commands/race/race.profile.tsx
+++ src/commands/race/race.profile.tsx
@@ -1,17 +1,58 @@
-import { JSX } from "react";
-import { MetaBody, RaceBody } from "@utils";
-import { Container, Header } from "@components";
+import { 
+  buildModifiers, 
+  convertCash, 
+  EventImages, 
+  filterModifiers, 
+  getTowers, 
+  MetaBody, 
+  RaceBody, 
+  splitUppercase 
+} from "@utils";
+import { Event, Layout } from "@components";
 
 export interface RaceProfileProps {
-    event: RaceBody;
-    metaData: MetaBody;
+  event: RaceBody;
+  metaData: MetaBody;
 }
 
 export function RaceProfile({ event, metaData }: RaceProfileProps): JSX.Element {
 
+  const raceIcon = EventImages.Race;
+  const modifiers = filterModifiers(buildModifiers(metaData));
+
+  const infoItems = [
+    { label: "Difficulty", value: metaData.difficulty },
+    { label: "Mode", value: metaData.mode },
+    { label: "Starting Cash", value: convertCash(metaData.startingCash), image: EventImages.Cash },
+    { label: "Starting Lives", value: metaData.lives, image: EventImages.Lives },
+    { label: "Start Round", value: metaData.startRound, image: EventImages.StartRound },
+    { label: "End Round", value: metaData.endRound, image: EventImages.StartRound }
+  ];
+
   return (
-    <Container background="#0e3f78">
-      <Header mode="Race" name={event.name} />
-    </Container>
+    <Layout.Container>
+      <Event.Header
+        eventType="Race"
+        eventName={splitUppercase(event.name)}
+      />
+      <Layout.Row>
+        <Event.Map
+          map={metaData.map}
+          iconPath={raceIcon}
+          width={480}
+          height={302}
+        />
+        <Layout.Column>
+          <Event.Info items={infoItems} />
+          <Event.Bar start={event.start} end={event.end} />
+        </Layout.Column>
+      </Layout.Row>
+      <Layout.Row>
+        <Event.Modifiers modifiers={modifiers} />
+        <Layout.Column>
+          <Event.Towers towers={getTowers(metaData._towers ?? [])} />
+        </Layout.Column>
+      </Layout.Row>
+    </Layout.Container>
   );
 }

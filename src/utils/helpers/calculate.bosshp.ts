7320e1c1d21a65e56dc94bd89ee71d520cdad34e
Lucy <xlucyyu@gmail.com>
2026-07-10 13:57:40 +0200

    Update calculate.bosshp.ts

--- src/utils/helpers/calculate.bosshp.ts
+++ src/utils/helpers/calculate.bosshp.ts
@@ -1,12 +1,3 @@
-import { Boss, bossHpValues } from "../assets";
-import { BossDifficulty } from "../types";
-
-interface BossTier {
-    tier: number;
-    totalHp: number;
-    skullHp: number;
-}
-
 const playerMultiplier: Record<number, number> = {
     1: 1,
     2: 1.2,
@@ -14,24 +5,32 @@ const playerMultiplier: Record<number, number> = {
     4: 1.6
 }
 
+interface BossTier {
+    tier: number;
+    totalHp: string;
+    skullHp: string;
+}
+
 export function calcBossHp(
-    boss: Boss, 
+    boss: string, 
     hpModifier: number, 
     playerCount: number, 
-    difficulty: BossDifficulty
+    difficulty: string
 ): BossTier[] {
 
-    const currentBoss = bossHpValues[difficulty][boss];
-    const shieldMultiplier = currentBoss.Shield ?? 1;
+    const currentBoss = bossHpValues.Elite.Dreadbloon
+    const shieldMultiplier = currentBoss.Shield ?? 0;
     const skullCount = currentBoss.Skulls;
 
     return currentBoss.TierHP.map((baseHp, index) => {
-        const totalHp = Math.round(baseHp * hpModifier * shieldMultiplier * playerMultiplier[playerCount]);
+        const totalHp = baseHp + skullCount * (baseHp * shieldMultiplier)
+        const baseHpPerSkull = Math.round((baseHp / skullCount) * hpModifier * playerMultiplier[playerCount]);
         const skullHp = Math.round(totalHp / skullCount);
         return {
             tier: index + 1,
-            totalHp: totalHp,
-            skullHp: skullHp
+            totalHp: totalHp.toLocaleString("en-US"),
+            baseHpPerSkull,
+            skullHp: skullHp.toLocaleString("en-US")
         }
     });
 }

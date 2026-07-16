7574b9959cea3c5047befa023dc1dfe3633be6f0
Lucy <xlucyyu@gmail.com>
2026-07-16 15:11:13 +0200

    Refactor CollectionCommand for cleaner structure

--- src/commands/collection/collection.command.tsx
+++ src/commands/collection/collection.command.tsx
@@ -1,51 +1,21 @@
-import { 
-    ApplicationIntegrationType,
-    ChatInputCommandInteraction,
-    InteractionContextType, 
-    SlashCommandBuilder 
-} from "discord.js";
-import { JSX } from "react";
 import { EventBody, EventType, InstaSchedule } from "@utils";
 import { BaseCommand } from "../base.command";
-import { ComponentState, CreateComponentState } from "@components";
-import { CurrentEventData, EventCacheEntry, eventManager } from "@manager";
 import { CollectionProfile } from "./collection.profile";
+import { EventCacheEntry } from "@manager";
 
 export type CollectionProps = EventCacheEntry<EventBody, InstaSchedule>;
 
-
 export class CollectionCommand extends BaseCommand<EventBody, InstaSchedule> {
 
-    public commandData = new SlashCommandBuilder()
-        .setName("collection")
-        .setDescription("shows the collection event data.")
-        .setIntegrationTypes(
-        ApplicationIntegrationType.GuildInstall,
-        ApplicationIntegrationType.UserInstall,
-        )
-        .setContexts(
-        InteractionContextType.Guild,
-        InteractionContextType.PrivateChannel
-        )
+  protected readonly eventType = EventType.Collection;
+  protected readonly urlKey = EventType.Collection;
 
-    public getProfile(event: CurrentEventData<EventBody, InstaSchedule>): JSX.Element {
+  public commandData = BaseCommand.baseSlashCommand("collection", "Show Collection Event Data.");
 
-        return CollectionProfile({
-            event: event.data,
-            metaData: event.metaData
-        });
-    }
-
-    public getEventProps(): CollectionProps | null {
-        return eventManager.getEventCache(EventType.Collection).getCache();
-    }
-
-    protected getInitialState(interaction: ChatInputCommandInteraction, eventProps: CollectionProps): ComponentState {
-
-        return CreateComponentState({
-            eventId: eventProps.currentEvent.data.id,
-            difficulty: null,
-            userId : interaction.user.id
-        })
-    }
+  public getProfile(event: CollectionProps["currentEvent"]): JSX.Element {
+    return CollectionProfile({
+      event: event.data,
+      metaData: event.metaData
+    })
+  }
 }

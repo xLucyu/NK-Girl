8256629b412d62bee66461c9e5fedf828b9cf0a5
Lucy <xlucyyu@gmail.com>
2026-07-21 13:22:08 +0200

    Update calculate.bosshp.ts

--- src/utils/helpers/calculate.bosshp.ts
+++ src/utils/helpers/calculate.bosshp.ts
@@ -1,4 +1,4 @@
-import { Boss, bossHpValues,  } from "../assets";
+import { Boss, bossHpValues } from "../assets";
 import { BossDifficulty } from "../types";
 
 export const playerMultiplier: Record<number, number> = {
@@ -8,10 +8,16 @@ export const playerMultiplier: Record<number, number> = {
   4: 1.6
 }
 
+interface BossHealthData {
+  skullCount: number;
+  tiers: BossTier[];
+}
+
 interface BossTier {
   tier: number;
-  totalHp: string;
-  skullHp: string;
+  baseHealth: number;
+  withShield: number;
+  totalHealth: number;
 }
 
 export function calcBossHp(
@@ -19,21 +25,29 @@ export function calcBossHp(
   hpModifier: number, 
   playerCount: number, 
   difficulty: BossDifficulty
-): BossTier[] {
+): BossHealthData {
 
-  const currentBoss = bossHpValues[difficulty][boss];
-  const shieldMultiplier = currentBoss?.Shield ?? 0;
-  const skullCount = currentBoss?.Skulls;
+  const currentBoss = bossHpValues[difficulty][boss]
+  const shieldMultiplier = currentBoss.Shield ?? 0;
+  const skullCount = currentBoss.Skulls;
 
-  return currentBoss.TierHP.map((baseHp, index) => {
-    const totalHp = baseHp + skullCount * (baseHp * shieldMultiplier)
-    const baseHpPerSkull = Math.round((baseHp / skullCount) * hpModifier * playerMultiplier[playerCount]);
-    const skullHp = Math.round(totalHp / skullCount);
+  const tiers = currentBoss.TierHP.map((base, index) => {
+    const baseHealth = base * hpModifier * playerMultiplier[playerCount];
+    const healthWithShield = Math.round(baseHealth * (skullCount * shieldMultiplier));
+    const maxHealth = baseHealth + healthWithShield;
+    const healthPerSkull = baseHealth / skullCount;
+    const shieldPerSkull = healthWithShield / skullCount;
+
     return {
       tier: index + 1,
-      totalHp: totalHp.toLocaleString("en-US"),
-      baseHpPerSkull,
-      skullHp: skullHp.toLocaleString("en-US")
+      baseHealth: healthPerSkull,
+      withShield: shieldPerSkull,
+      totalHealth: maxHealth,
     }
   });
+
+  return {
+    skullCount,
+    tiers
+  }
 }

34042388e5d5f7d729275b67755dc427811def1e
Lucy <xlucyyu@gmail.com>
2026-07-23 10:08:21 +0200

    Update collection.ts

--- src/event-manager/cache/events/collection.ts
+++ src/event-manager/cache/events/collection.ts
@@ -1,48 +1,48 @@
 import { getData } from "@api";
 import { 
-    getCollectionCycle,
-    InstaSchedule, 
-    EventBody,
-    EventType,
-    API_URLS,
-    NkData
+  getCollectionCycle,
+  InstaSchedule, 
+  EventBody,
+  EventType,
+  API_URLS,
+  NkData
 } from "@utils";
 import { BaseEventCache, PreviousEvent } from "./base";
 
 export class CollectionCache extends BaseEventCache<EventBody, InstaSchedule> {
 
-    protected eventType = EventType.Collection;
-    protected url = API_URLS.Events; // only place available for collection
+  protected eventType = EventType.Collection;
+  protected url = API_URLS.Events; // only place available for collection
 
-    protected async getEventData(): Promise<EventBody[]> {
+  protected async getEventData(): Promise<EventBody[]> {
+      
+    const data = await getData<NkData<EventBody>>(this.url);
+    return data.body;
+  }
+
+  protected getCurrentActiveEvent(events: EventBody[], now: number, firstUse: boolean): EventBody {
         
-        const data = await getData<NkData<EventBody>>(this.url);
-        return data.body;
+    let currentEvent: EventBody | undefined;
+    
+    if (firstUse) {
+      currentEvent = events.find((event) => event.type == "collectableEvent");
+    } else {
+      currentEvent = events.find((event) => event.end > now && event.type == "collectableEvent");
     }
 
-    protected getCurrentActiveEvent(events: EventBody[], now: number, firstUse: boolean): EventBody {
-        
-        let currentEvent: EventBody | undefined;
-        
-        if (firstUse) {
-            currentEvent = events.find((event) => event.type == "collectableEvent");
-        } else {
-            currentEvent = events.find((event) => event.end > now && event.type == "collectableEvent");
-        }
+    if (!currentEvent) throw new Error();
+    return currentEvent;
+  }
 
-        if (!currentEvent) throw new Error();
-        return currentEvent;
-    }
+  protected async getMetaData(event: EventBody): Promise<InstaSchedule> {
+    return getCollectionCycle(event);
+  }
 
-    protected async getMetaData(event: EventBody): Promise<InstaSchedule> {
-        return getCollectionCycle(event);
-    }
+  protected override getPreviousEvents(): PreviousEvent[] {
+    return [];
+  }
 
-    protected override getPreviousEvents(): PreviousEvent[] {
-        return [];
-    }
-
-    protected getBucketPath(event: EventBody): string {
-        return `Event/Collection/${event.id}/event.json`;
-    }
+  protected getBucketPath(event: EventBody): string {
+    return `Event/Collection/${event.id}/event.json`;
+  }
 }

7c6ca7fba8fceaf63b0049724d0b5c696e06ecf6
Lucy <xlucyyu@gmail.com>
2026-07-24 11:07:22 +0200

    Update index.ts

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -4,30 +4,26 @@ import * as sync from "./sync/sync.command";
 import * as help from "./help/help.command";
 import * as race from "./race/race.command";
 import * as odyssey from "./odyssey/odyssey.command";
+import * as tile from "./ct/tile.command";
 
 export const eventCommands = {
-    boss: new boss.BossCommand(),
-    race: new race.RaceCommand(),
-    odyssey: new odyssey.OdysseyCommand(),
-    boss_details: new boss_details.BossDetailsCommand()
+  boss: new boss.BossCommand(),
+  boss_details: new boss_details.BossDetailsCommand(),
+  race: new race.RaceCommand(),
+  odyssey: new odyssey.OdysseyCommand(),
+  tile: new tile.TileCommand()
 }
 
 export const helperCommands = {
-    sync: new sync.SyncCommand(),
-    help: new help.HelpCommand()
+  sync: new sync.SyncCommand(),
+  help: new help.HelpCommand()
 }
 
 export const commands = {
-    ...eventCommands,
-    ...helperCommands
+  ...eventCommands,
+  ...helperCommands
 }
 
 export * from "./interaction";
 export * from "./base.command";
 export * from "./cooldown";
-
-export { BossProfile } from "./boss/boss.profile";
-export { RaceProfile } from "./race/race.profile";
-export { OdysseyProfile } from "./odyssey/odyssey.profile";
-export { BossDetailsProfile } from "./boss/boss-details.profile";
- 

9f0d6a2d5704f38ebf588aa0f290e5706d826a64
Lucy <xlucyyu@gmail.com>
2026-07-24 11:15:47 +0200

    Update index.ts

--- src/components/react/layout/index.ts
+++ src/components/react/layout/index.ts
@@ -2,10 +2,12 @@ import { Box } from "./Box";
 import { Column } from "./Column";
 import { Container } from "./Container";
 import { Row } from "./Row";
+import { Fit } from "./Fit";  
 
 export const Layout = {
-    Box: Box,
-    Column: Column,
-    Container: Container,
-    Row: Row
+  Box: Box,
+  Column: Column,
+  Container: Container,
+  Row: Row,
+  Fit: Fit
 }

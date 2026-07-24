c5684829cd11f7c6554c40b2ca8261afd2175ae8
Lucy <xlucyyu@gmail.com>
2026-07-24 11:04:56 +0200

    Update tile.profile.tsx

--- src/commands/tile/tile.profile.tsx
+++ src/commands/tile/tile.profile.tsx
@@ -1,5 +1,6 @@
 import { Event, Layout } from "@components";
 import { 
+  Boss,
   buildCTModifiers, 
   convertCash, 
   CTBody, 
@@ -22,6 +23,33 @@ const liveCount: Record<string, number> = {
   "Hard": 100
 }
 
+const subGameType: Record<number, string> = {
+  2: "Race",
+  4: "Boss",
+  8: "LeastTiers",
+  9: "LeastCash"
+}
+
+const bossType: Record<number, Boss> = {
+  0: Boss.Bloonarius,
+  1: Boss.Lych,
+  2: Boss.Vortex,
+  3: Boss.Dreadbloon,
+  4: Boss.Phayze,
+  5: Boss.Blastapopoulos,
+  6: Boss.Diamondback
+}
+
+const getTitle = (tile: TileCode): string => {
+  if (tile.GameData.bossData) return `${bossType[tile.GameData.bossData.bossBloon]} ${tile.GameData.bossData.TierCount} Tier`;
+  return splitUppercase(subGameType[tile.GameData.subGameType]);
+}
+
+const getEndRound = (tile: TileCode): number => {
+  if (tile.GameData.bossData) return tile.GameData.bossData.TierCount * 20 + 20;
+  return tile.GameData.dcModel.startRules.endRound;
+}
+
 export function TileProfile({ event, tile }: CtProps): JSX.Element {
 
   const ctNumber = tile.EventNumber;
@@ -34,7 +62,7 @@ export function TileProfile({ event, tile }: CtProps): JSX.Element {
     { label: "Starting Cash", value: convertCash(tile.GameData.dcModel.startRules.cash), image: EventImages.Cash },
     { label: "Starting Lives", value: liveCount[tile.GameData.selectedDifficulty], image: EventImages.Lives },
     { label: "Start Round", value: tile.GameData.dcModel.startRules.round, image: EventImages.StartRound },
-    { label: "End Round", value: tile.GameData.dcModel.startRules.endRound, image: EventImages.StartRound }
+    { label: "End Round", value: getEndRound(tile), image: EventImages.StartRound }
   ];
 
   return (
@@ -42,16 +70,19 @@ export function TileProfile({ event, tile }: CtProps): JSX.Element {
     <Event.Header
       eventType={EventType.CT}
       eventName={`Contested Territory #${ctNumber}`}
+      difficulty={getTitle(tile)}
     />
-    <Layout.Row>
-      <Layout.Box style={{ flex: 1 }}>
+    <Layout.Row style={{ alignItems: "stretch", height: 300 }}>
+      <Layout.Box style={{ flex: 1}}>
         <Event.Map
           map={tile.GameData.selectedMap}
           iconPath={ctIcon}
         />
       </Layout.Box>
       <Layout.Column flex={1}>
+        <div style={{ flex: 1, display: "flex" }}>
         <Event.Info items={infoItems} />
+        </div>
         <Event.Bar start={event.start} end={event.end} />
       </Layout.Column>
     </Layout.Row>

fcdc4751f65435d01cad0906c74403ee4ad279dd
Lucy <xlucyyu@gmail.com>
2026-07-27 08:42:27 +0200

    Update button.menu.ts

--- src/components/discord/button.menu.ts
+++ src/components/discord/button.menu.ts
@@ -1,24 +1,24 @@
 import {
-    ActionRowBuilder,
-    ButtonBuilder,
-    ButtonStyle
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle
 } from "discord.js";
 
 export function BuildButtonMenu(args: {
-    buttons: {
-        customId: string;
-        label: string;
-        style: ButtonStyle;
-        disabled?: boolean;
-    }[];
+  buttons: {
+    customId: string;
+    label: string;
+    style: ButtonStyle;
+    disabled?: boolean;
+  }[];
 }) {
-    return new ActionRowBuilder<ButtonBuilder>().addComponents(
-        args.buttons.map((button) => 
-        new ButtonBuilder()
-            .setCustomId(button.customId)
-            .setLabel(button.label)
-            .setStyle(button.style)
-            .setDisabled(button.disabled ?? false)   
-        )
+  return new ActionRowBuilder<ButtonBuilder>().addComponents(
+    args.buttons.map((button) => 
+     new ButtonBuilder()
+      .setCustomId(button.customId)
+      .setLabel(button.label)
+      .setStyle(button.style)
+      .setDisabled(button.disabled ?? false)   
     )
+  )
 }

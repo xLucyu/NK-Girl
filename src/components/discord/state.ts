7c6c8c2adef2f0b5656bb3ed8b26de6c95b72e32
Lucy <xlucyyu@gmail.com>
2026-07-28 12:31:34 +0200

    Update state.ts

--- src/components/discord/state.ts
+++ src/components/discord/state.ts
@@ -1,33 +1,25 @@
-import type { Boss } from "@utils";
-
 export const TIMEOUT = 3 * 60 * 1000; // 3 minutes
 
-export interface Options {
-  difficulty?: string;
-  playerCount?: number;
-  boss?: Boss;
-  hpModifier?: number;
-  tileCode?: string;
-}
+export interface BaseOptions {
+  [key: string]: unknown;
+} 
 
-export type ComponentState = {
+export interface ComponentState<O extends BaseOptions = BaseOptions> {
   event: string;
+  options: O;
   userId: string;
-  options: Options;
   expiresAt: number;
 };
 
 export const componentState = new Map<string, ComponentState>();
 
-export function CreateComponentState(paramters: {
+export function CreateComponentState<O extends BaseOptions>(parameters: {
   event: string;
+  options: O;
   userId: string;
-  options?: Options;
-}): ComponentState {
+}): ComponentState<O> {
   return {
-    event: paramters.event,
-    userId: paramters.userId,
-    options: paramters.options ?? {},
+    ...parameters,
     expiresAt: Date.now() + TIMEOUT,
   };
 }

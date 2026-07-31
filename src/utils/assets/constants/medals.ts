a666b5b905bfdff5d4bfaea1d604007edbb05a36
Lucy <xlucyyu@gmail.com>
2026-07-31 11:35:44 +0200

    Update medals.ts

--- src/utils/assets/constants/medals.ts
+++ src/utils/assets/constants/medals.ts
@@ -1,3 +1,5 @@
+import { MedalImages } from "../images";
+
 export type MedalsMode = "Race" | "Standard" | "Elite" | "CTplayer" | "CTteam";
 
 export type MedalEntry = {
@@ -9,65 +11,61 @@ export type MedalEntry = {
 export const MEDALS: Record<MedalsMode, MedalEntry[]> = {
     
   Race: [
-    { min: 1, max: 1, medal: "RaceFirst" },
-    { min: 2, max: 2, medal: "RaceSecond" },
-    { min: 3, max: 3, medal: "RaceThird" },
-    { min: 4, max: 50, medal: "RaceT50" },
-    { min: 51, max: 100, medal: "RaceT100" },
-    { min: 0.00, max: 0.10, medal: "RaceT10Perc" },
-    { min: 0.10, max: 0.25, medal: "RaceT25Perc" },
-    { min: 0.25, max: 0.50, medal: "RaceT50Perc" },
-    { min: 0.50, max: 0.75, medal: "RaceT75Perc" },
-    { min: 0.75, max: 1.0, medal: "Participant" }
+    { min: 1, max: 1, medal: MedalImages.Race.First },
+    { min: 2, max: 2, medal: MedalImages.Race.Second },
+    { min: 3, max: 3, medal: MedalImages.Race.Third },
+    { min: 4, max: 50, medal: MedalImages.Race.Top50 },
+    { min: 51, max: 100, medal: MedalImages.Race.Top100 },
+    { min: 0.00, max: 0.10, medal: MedalImages.Race.Top10Percent },
+    { min: 0.10, max: 0.25, medal: MedalImages.Race.Top25Percent },
+    { min: 0.25, max: 0.50, medal: MedalImages.Race.Top50Percent },
+    { min: 0.50, max: 0.75, medal: MedalImages.Race.Top75Percent },
   ],
 
   Standard: [
-    { min: 1, max: 1, medal: "BossFirst" },
-    { min: 2, max: 2, medal: "BossSecond" },
-    { min: 3, max: 3, medal: "BossThird" },
-    { min: 4, max: 50, medal: "BossT50" },
-    { min: 51, max: 100, medal: "BossT100" },
-    { min: 0.00, max: 0.10, medal: "BossT10Perc" },
-    { min: 0.10, max: 0.25, medal: "BossT25Perc" },
-    { min: 0.25, max: 0.50, medal: "BossT50Perc" },
-    { min: 0.50, max: 0.75, medal: "BossT75Perc" },
-    { min: 0.75, max: 1.0, medal: "Participant" }
+    { min: 1, max: 1, medal: MedalImages.Boss.Standard.First },
+    { min: 2, max: 2, medal: MedalImages.Boss.Standard.Second },
+    { min: 3, max: 3, medal: MedalImages.Boss.Standard.Third },
+    { min: 4, max: 50, medal: MedalImages.Boss.Standard.Top50 },
+    { min: 51, max: 100, medal: MedalImages.Boss.Standard.Top100 },
+    { min: 0.00, max: 0.10, medal: MedalImages.Boss.Standard.Top10Percent },
+    { min: 0.10, max: 0.25, medal: MedalImages.Boss.Standard.Top25Percent },
+    { min: 0.25, max: 0.50, medal: MedalImages.Boss.Standard.Top50Percent },
+    { min: 0.50, max: 0.75, medal: MedalImages.Boss.Standard.Top75Percent },
+  
   ],
 
   Elite: [
-    { min: 1, max: 1, medal: "BossEFirst" },
-    { min: 2, max: 2, medal: "BossESecond" },
-    { min: 3, max: 3, medal: "BossEThird" },
-    { min: 4, max: 50, medal: "BossET50" },
-    { min: 51, max: 100, medal: "BossET100" },
-    { min: 0.00, max: 0.10, medal: "BossET10Perc" },
-    { min: 0.10, max: 0.25, medal: "BossET25Perc" },
-    { min: 0.25, max: 0.50, medal: "BossET50Perc" },
-    { min: 0.50, max: 0.75, medal: "BossET75Perc" },
-    { min: 0.75, max: 1.0, medal: "Participant" }
+    { min: 1, max: 1, medal: MedalImages.Boss.Elite.First },
+    { min: 2, max: 2, medal: MedalImages.Boss.Elite.Second },
+    { min: 3, max: 3, medal: MedalImages.Boss.Elite.Third },
+    { min: 4, max: 50, medal: MedalImages.Boss.Elite.Top50 },
+    { min: 51, max: 100, medal: MedalImages.Boss.Elite.Top100 },
+    { min: 0.00, max: 0.10, medal: MedalImages.Boss.Elite.Top10Percent },
+    { min: 0.10, max: 0.25, medal: MedalImages.Boss.Elite.Top25Percent },
+    { min: 0.25, max: 0.50, medal: MedalImages.Boss.Elite.Top50Percent },
+    { min: 0.50, max: 0.75, medal: MedalImages.Boss.Elite.Top75Percent },
   ],
 
   CTplayer: [
-    { min: 1, max: 25, medal: "CTPT25" },
-    { min: 26, max: 100, medal: "CTPT100" },
-    { min: 0.00, max: 0.01, medal: "CTPT1Perc" },
-    { min: 0.01, max: 0.10, medal: "CTPT10Perc" },
-    { min: 0.10, max: 0.25, medal: "CTPT25Perc" },
-    { min: 0.25, max: 0.50, medal: "CTPT50Perc" },
-    { min: 0.50, max: 0.75, medal: "CTPT75Perc" },
-    { min: 0.75, max: 1.0, medal: "Participant" }
+    { min: 1, max: 25, medal: MedalImages.CT.Player.Top25 },
+    { min: 26, max: 100, medal: MedalImages.CT.Player.Top100 },
+    { min: 0.00, max: 0.01, medal: MedalImages.CT.Player.Top1Percent },
+    { min: 0.01, max: 0.10, medal: MedalImages.CT.Player.Top10Percent },
+    { min: 0.10, max: 0.25, medal: MedalImages.CT.Player.Top25Percent },
+    { min: 0.25, max: 0.50, medal: MedalImages.CT.Player.Top50Percent },
+    { min: 0.50, max: 0.75, medal: MedalImages.CT.Player.Top75Percent },
   ],
 
   CTteam: [
-    { min: 1, max: 1, medal: "CTTFirst" },
-    { min: 2, max: 2, medal: "CTTSecond" },
-    { min: 3, max: 3, medal: "CTTThird" },
-    { min: 4, max: 25, medal: "CTTT25" },
-    { min: 26, max: 100, medal: "CTTT100" },
-    { min: 0.00, max: 0.01, medal: "CTTT1Perc" },
-    { min: 0.01, max: 0.10, medal: "CTTT10Perc" },
-    { min: 0.10, max: 0.25, medal: "CTTT25Perc" },
-    { min: 0.25, max: 0.75, medal: "CTTT75Perc" },
-    { min: 0.75, max: 1.0, medal: "Participant" }
+    { min: 1, max: 1, medal: MedalImages.CT.Teams.First },
+    { min: 2, max: 2, medal: MedalImages.CT.Teams.Second },
+    { min: 3, max: 3, medal: MedalImages.CT.Teams.Third },
+    { min: 4, max: 25, medal: MedalImages.CT.Teams.Top25 },
+    { min: 26, max: 100, medal: MedalImages.CT.Teams.Top100 },
+    { min: 0.00, max: 0.01, medal: MedalImages.CT.Teams.Top1Percent },
+    { min: 0.01, max: 0.10, medal: MedalImages.CT.Teams.Top10Percent },
+    { min: 0.10, max: 0.25, medal: MedalImages.CT.Teams.Top25Percent },
+    { min: 0.25, max: 0.75, medal: MedalImages.CT.Teams.Top75Percent },
   ]
 };

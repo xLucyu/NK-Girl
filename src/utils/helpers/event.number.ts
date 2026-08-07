5c4002458dcc1622676a9ab76ffa4137aded7ccf
Lucy <xlucyyu@gmail.com>
2026-08-07 08:50:58 +0200

    Refactor event number calculation logic

--- src/utils/helpers/event.number.ts
+++ src/utils/helpers/event.number.ts
@@ -1,18 +1,23 @@
-const eventsByFirstTimeStampAndDuration: Record<string, [number, number]> = {
-  Race: [1544601600000, 7],
-  Standard: [1533974400000, 1],
-  Advanced: [1535097600000, 1],
-  CT: [1660082400000, 14],
-  Odyssey: [1593532800000, 7],
-};
+interface EventTimeConfig {
+  mode: string;
+  firstEvent: number;
+  duration: number;
+}
 
-export function getNumberForEvent(eventStart: number, mode: string): number | null {
+const eventsTimeStamps: EventTimeConfig[] = [
+  { mode: "Race", firstEvent: 1544601600000, duration: 7 },
+  { mode: "Standard",firstEvent: 1533974400000, duration: 1 },
+  { mode: "Advanced", firstEvent: 1535097600000, duration: 1 },
+  { mode: "CT", firstEvent: 1660082400000, duration: 14 },
+  { mode: "BossRush", firstEvent: 1712786400000, duration: 14 },
+  { mode: "Odyssey", firstEvent: 1593532800000, duration: 7 }
+];
 
-  if (!(mode in eventsByFirstTimeStampAndDuration)) return null;
+export function getNumberForEvent(eventStart: number, mode: string): number {
 
-  const [firstTimeStamp, duration] = eventsByFirstTimeStampAndDuration[mode];
-  const timeDifference = eventStart - firstTimeStamp;
-  const calculateNumber = Math.floor(timeDifference / (duration * 24 * 60 * 60 * 1000));
+  const selectedEvent = eventsTimeStamps.find((event) => event.mode === mode)!; // will always be found
 
-  return Math.max(0, calculateNumber);
+  const timeDifference = eventStart - selectedEvent.firstEvent;
+  const calculate = Math.floor(timeDifference / (selectedEvent.duration * 24 * 60 * 60 * 1000));
+  return Math.max(0, calculate);
 }

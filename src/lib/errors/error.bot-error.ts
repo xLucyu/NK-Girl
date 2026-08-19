748323bc4c884001dd24c2f84fc3a82d4314dabe
Lucy <xlucyyu@gmail.com>
2026-08-19 15:26:26 +0200

    Add BotError class for error handling

---
+++ src/lib/errors/error.bot-error.ts
@@ -0,0 +1,11 @@
+export class BotError extends Error {
+
+  public readonly title: string;
+  public readonly userMessage: string;
+
+  constructor(title: string, userMessage: string) {
+    super(userMessage);
+    this.title = title;
+    this.userMessage = userMessage;
+  }
+}

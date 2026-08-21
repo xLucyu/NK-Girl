494aad8713b157372c410a5aa93aa5442b0a5fc8
Lucy <xlucyyu@gmail.com>
2026-08-21 15:50:46 +0200

    Add files via upload

---
+++ src/btd6/assets/events/index.ts
@@ -0,0 +1,12 @@
+export const EventImages = {
+  BossChallenge: "events/BossChallenge.png",
+  Cash: "events/cash.png",
+  Collection: "events/collection.png",
+  CT: "events/ct.png",
+  LeastCash: "events/leastcash.png",
+  Lives: "events/Lives.png",
+  Odyssey: "events/OdysseyCrewBtn.png",
+  LeastTiers: "events/leasttiers.png",
+  Race: "events/race.png",
+  StartRound: "events/StartRoundIcon.png",
+};
---
+++ src/btd6/assets/medals/index.ts
@@ -0,0 +1,65 @@
+export const MedalImages = {
+  Boss: {
+    Elite: {
+      First: "medals/boss/elite/first.png",
+      Second: "medals/boss/elite/second.png",
+      Third: "medals/boss/elite/third.png",
+      Top50: "medals/boss/elite/top50.png",
+      Top100: "medals/boss/elite/top100.png",
+      Top10Percent: "medals/boss/elite/top10_percent.png",
+      Top25Percent: "medals/boss/elite/top25_percent.png",
+      Top50Percent: "medals/boss/elite/top50_percent.png",
+      Top75Percent: "medals/boss/elite/top75_percent.png",
+    },
+
+    Standard: {
+      First: "medals/boss/standard/first.png",
+      Second: "medals/boss/standard/second.png",
+      Third: "medals/boss/standard/third.png",
+      Top50: "medals/boss/standard/top50.png",
+      Top100: "medals/boss/standard/top100.png",
+      Top10Percent: "medals/boss/standard/top10_percent.png",
+      Top25Percent: "medals/boss/standard/top25_percent.png",
+      Top50Percent: "medals/boss/standard/top50_percent.png",
+      Top75Percent: "medals/boss/standard/top75_percent.png",
+    },
+  },
+
+  CT: {
+    Player: {
+      Top25: "medals/ct/player/top25.png",
+      Top100: "medals/ct/player/top100.png",
+      Top1Percent: "medals/ct/player/top1_percent.png",
+      Top10Percent: "medals/ct/player/top10_percent.png",
+      Top25Percent: "medals/ct/player/top25_percent.png",
+      Top50Percent: "medals/ct/player/top50_percent.png",
+      Top75Percent: "medals/ct/player/top75_percent.png",
+    },
+
+    Teams: {
+      First: "medals/ct/teams/first.png",
+      Second: "medals/ct/teams/second.png",
+      Third: "medals/ct/teams/third.png",
+      Top25: "medals/ct/teams/top25.png",
+      Top100: "medals/ct/teams/top100.png",
+      Top1Percent: "medals/ct/teams/top1_percent.png",
+      Top10Percent: "medals/ct/teams/top10_percent.png",
+      Top25Percent: "medals/ct/teams/top25_percent.png",
+      Top75Percent: "medals/ct/teams/top75_percent.png"
+    },
+  },
+
+  Race: {
+    First: "medals/race/first.png",
+    Second: "medals/race/second.png",
+    Third: "medals/race/third.png",
+    Top50: "medals/race/top50.png",
+    Top100: "medals/race/top100.png",
+    Top10Percent: "medals/race/top10_percent.png",
+    Top25Percent: "medals/race/top25_percent.png",
+    Top50Percent: "medals/race/top50_percent.png",
+    Top75Percent: "medals/race/top75_percent.png",
+  },
+
+  Participation: "medals/participation.png"
+};
